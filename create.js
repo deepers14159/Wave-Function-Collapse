45f346d3b7ee8a78c91d8ab78c90c1ddda44cc45
deepers14159 <deepers14159@users.noreply.github.com>
2019-04-14 22:28:38 -0400

    A somewhat working version.

--- create.js
+++ create.js
@@ -1,3 +1,5 @@
+var numCompleted;
+
 function create()
 {
   initMap();
@@ -5,20 +7,197 @@ function create()
   initRules();
 
   initMap2();
+
+  //while(numCompleted != size.x * size.y)
+  //{
+  //for(var i = 0; i < 100; i++)
+
+  //}
+}
+function update()
+{
+  sizeAdjust();
+  updateOnePixel();
+  draw();
+}
+function updateOnePixel()
+{
+  var coordLE = getLowestEntropy();
+  //while(coordLE.x != -1)
+  //{
+    collaspe(coordLE);
+    //coordLE = getLowestEntropy();
+  //}
+  //numCompleted++;
+}
+function collaspe(coord)
+{
+  var dist = [];
+  var realVal;
+  for(var i = 0; i < numTiles; i++)
+  {
+    dist[i] = 0;
+    if(map[coord.x][coord.y][i] == true)
+      dist[i] = id2weight[i];
+  }
+  realVal = getRandOnDistribution(dist);
+  for(var i = 0; i < numTiles; i++)
+  {
+    map[coord.x][coord.y][i] = false;
+    if(i == realVal)
+      map[coord.x][coord.y][i] = true;
+  }
+
+  mapSet[coord.x][coord.y] = true;
+
+  upspot = [];
+  downspot = [];
+  rightspot = [];
+  leftspot = [];
+
+  for(var i = 0; i < numTiles; i++)
+  {
+    upspot[i] = false;
+    downspot[i] = false;
+    rightspot[i] = false;
+    leftspot[i] = false;
+  }
+
+  for(rule in rules)
+  {
+    if(rules[rule].direction == UP && rules[rule].a == realVal)
+      upspot[rules[rule].b] = true;
+    if(rules[rule].direction == DOWN && rules[rule].a == realVal)
+      downspot[rules[rule].b] = true;
+    if(rules[rule].direction == RIGHT && rules[rule].a == realVal)
+      rightspot[rules[rule].b] = true;
+    if(rules[rule].direction == LEFT && rules[rule].a == realVal)
+      leftspot[rules[rule].b] = true;
+  }
+
+  for(var i = 0; i < numTiles; i++)
+  {
+    if(upspot[i] == false)
+      setMap(getMovedCoord(coord, UP), i, false);
+    if(downspot[i] == false)
+      setMap(getMovedCoord(coord, DOWN), i, false);
+    if(rightspot[i] == false)
+      setMap(getMovedCoord(coord, RIGHT), i, false);
+    if(leftspot[i] == false)
+      setMap(getMovedCoord(coord, LEFT), i, false);
+  }
+
+  var upcount = 0;
+  var downcount = 0;
+  var rightcount = 0;
+  var leftcount = 0;
+
+  for(var i = 0; i < numTiles; i++)
+  {
+    if(getMap(getMovedCoord(coord, UP), i) == true)
+      upcount++;
+    if(getMap(getMovedCoord(coord, DOWN), i) == true)
+      downcount++;
+    if(getMap(getMovedCoord(coord, RIGHT), i) == true)
+      rightcount++;
+    if(getMap(getMovedCoord(coord, LEFT), i) == true)
+      leftcount++;
+  }
+
+  if(upcount == 1)
+    mapSet[getMovedCoord(coord, UP).x][getMovedCoord(coord, UP).y] = true;
+  if(downcount == 1)
+    mapSet[getMovedCoord(coord, DOWN).x][getMovedCoord(coord, DOWN).y] = true;
+  if(rightcount == 1)
+    mapSet[getMovedCoord(coord, RIGHT).x][getMovedCoord(coord, RIGHT).y] = true;
+  if(leftcount == 1)
+    mapSet[getMovedCoord(coord, LEFT).x][getMovedCoord(coord, LEFT).y] = true;
+
+  if(upcount == 0 || downcount == 0 || leftcount == 0 || rightcount == 0)
+  {
+    console.log("CONTRADICTION");
+    return;
+  }
+
+  original = [false, true, true, false] //false is already taken count
+  newc = [false, true, true] // set org array to false, make all the rules to true
+  //take the union of the falses
+}
+function getLowestEntropy()
+{
+  var numSame = 0;
+  var currentEntropy = -1;
+  var currentCoord = {x : 0, y : 0};
+
+  for(var i = 0; i<size.x; i++)
+  {
+    for(var j = 0; j<size.y; j++)
+    {
+      currentEntropy = getEntropy(map[i][j]);
+      currentCoord.x = i;
+      currentCoord.y = j;
+      if(mapSet[i][j] == false)
+        break;
+    }
+  }
+  if(currentEntropy == -1)
+    return {x : -1, y : -1};
+
+  var entropyij;
+
+  for(var i = 0; i<size.x; i++)
+  {
+    mapEntropy[i] = [];
+
+    for(var j = 0; j<size.y; j++)
+    {
+      mapEntropy[i][j] = getEntropy(map[i][j]);
+      if(mapSet[i][j] == true)
+        continue;
+
+      entropyij = getEntropy(map[i][j]);
+
+      if(entropyij < currentEntropy)
+      {
+        numSame = 0;
+        currentEntropy = entropyij;
+        currentCoord.x = i;
+        currentCoord.y = j;
+      }
+
+      if(floatEquals(entropyij, currentEntropy, 0.000001))
+      {
+        numSame++;
+        if(Math.random() < 1 / (numSame +1))
+        {
+          currentCoord.x = i;
+          currentCoord.y = j;
+        }
+      }
+
+    }
+  }
+  //console.log(currentCoord);
+  return currentCoord;
 }
 function initMap2()
 {
   for(var i = 0; i<size.x; i++)
   {
+    mapSet[i] = [];
     for(var j = 0; j<size.y; j++)
     {
       map[i][j] = [];
+      mapSet[i][j] = false;
       for(var k = 0; k < numTiles; k++)
       {
         map[i][j][k] = true;
       }
     }
   }
+  numCompleted = 0;
+  if(numTiles == 1)
+    numCompleted = size.x * size.y;
 }
 function initRules()
 {
@@ -60,11 +239,13 @@ function initSourceDictionary()
     source[i] = [];
     for(var j = 0; j < rawSource[0].length; j++)
     {
-      console.log(rawSource[i][j]);
-      console.log(!(rawSource[i][j] in color2id));
+      //console.log(rawSource[i][j]);
+      //console.log(!(rawSource[i][j] in color2id));
       if(!(rawSource[i][j] in color2id))
       {
         color2id[rawSource[i][j]] = Object.keys(color2id).length;
+        id2color[Object.keys(color2id).length-1] = rawSource[i][j];
+
         id2weight[color2id[rawSource[i][j]]] = 1;
       }
       else
--- utility.js
+++ utility.js
@@ -4,12 +4,122 @@ const LEFT = "left";
 const RIGHT = "right";
 
 var map = [];
+var mapSet = [];
+var mapEntropy = [];
 var source = [];
 var color2id = {};
+var id2color = {};
 var id2weight = {};
 var rules = {};
 
 var numTiles;
+
+function floatEquals(x, y, error)
+{
+  if(Math.abs(x - y) < error)
+    return true;
+  return false;
+}
+function getEntropy(isUsed)
+{
+  var sum_of_weights = 0;
+  var sum_of_weighted_weights = 0;
+  var count = 0;
+
+  for(var i = 0; i < numTiles; i++)
+  {
+    if(isUsed[i])
+    {
+      sum_of_weights += id2weight[i];
+      sum_of_weighted_weights += id2weight[i] * Math.log(id2weight[i]);
+      count++;
+    }
+  }
+
+  if(count == 0)
+    return -1;
+  return Math.log(sum_of_weights) - sum_of_weighted_weights/sum_of_weights;
+}
+
+function getRandOnDistribution(dist)
+  {
+      var sum = 0;
+
+       for(var i = 0; i < dist.length; i++)
+          sum += dist[i];
+
+      var rand = Math.random()*sum;
+
+      var count = 0;
+      var checker = dist[0];
+
+      while(rand > checker)
+          {
+              count++;
+              checker += dist[count];
+          }
+
+      return count;
+  }
+
+function getColorAverage(isUsed)
+{
+  var count = 0;
+  var colorAvg = {r : 0, g : 0, b : 0};
+  var tempColor;
+  for(var i = 0; i < numTiles; i++)
+  {
+    if(isUsed[i])
+    {
+      //console.log(i);
+      //console.log(id2color[i]);
+      //console.log(string2rgb(id2color[i]));
+
+      tempColor = string2rgb(id2color[i]);
+      colorAvg.r += tempColor.r;
+      colorAvg.g += tempColor.g;
+      colorAvg.b += tempColor.b;
+      count++;
+    }
+  }
+  colorAvg.r = Math.round(colorAvg.r/count);
+  colorAvg.g = Math.round(colorAvg.g/count);
+  colorAvg.b = Math.round(colorAvg.b/count);
+
+  return rgb2string(colorAvg);
+}
+function rgb2string(color)
+{
+  out = "#";
+  out+= decimal2hexchar(Math.floor(color.r/16));
+  out+= decimal2hexchar(color.r%16);
+  out+= decimal2hexchar(Math.floor(color.g/16));
+  out+= decimal2hexchar(color.g%16);
+  out+= decimal2hexchar(Math.floor(color.b/16));
+  out+= decimal2hexchar(color.b%16);
+  return out;
+}
+function string2rgb(colorString)
+{
+  color = {r : 0, g : 0, b : 0};
+  color.r = hexchar2decimal(colorString.charAt(1)) * 16 + hexchar2decimal(colorString.charAt(2));
+  color.g = hexchar2decimal(colorString.charAt(3)) * 16 + hexchar2decimal(colorString.charAt(4));
+  color.b = hexchar2decimal(colorString.charAt(5)) * 16 + hexchar2decimal(colorString.charAt(6));
+  return color;
+}
+function hexchar2decimal(hexchar)
+{
+  var out = hexchar.charCodeAt(0) - "0".charCodeAt(0);
+  if(out >= 0 && out <=9)
+    return out;
+  return hexchar.charCodeAt(0) - "A".charCodeAt(0) + 10;
+}
+function decimal2hexchar(decimal)
+{
+  if(decimal >= 0 && decimal <=9)
+    return String.fromCharCode(decimal +  "0".charCodeAt(0));
+  return String.fromCharCode(decimal + "A".charCodeAt(0)-10);
+}
 
 class Rule {
   constructor(a, b, direction)
@@ -71,17 +181,17 @@ function isVaidSource(coord)
       return true;
   return false;
 }
-function setMap(coord, value)
+function setMap(coord, id, value)
 {
   if(!isVaid(coord))
     return;
-  map[coord.x][coord.y] = value;
+  map[coord.x][coord.y][id] = value;
 }
-function getMap(coord)
+function getMap(coord, id)
 {
   if(!isVaid(coord))
     return null;
-  return map[coord.x][coord.y];
+  return map[coord.x][coord.y][id];
 }
 function getMovedCoordSource(coord, direction)
 {
